d3ad0cefaccd637f2b9258bc21af0022cddd6fc0
Ryazanov Maxim <ryazanovms@yandex.ru>
2018-05-06 18:15:41 +0300

    Delete classes add rotation

--- animation.py
+++ animation.py
@@ -1,90 +1,87 @@
 import pygame as pg
 from pygame.locals import *
-import random, math
+import  math
 
 TICKS = 60 
 BACKGROUND = (220, 220, 220)
 SIZE = (800, 600)
 
-class Ball(pg.sprite.Sprite):
-    def __init__(self,coor=(0,0) , radius=50):
-        super(Ball, self).__init__()
-        self.x = coor[0]
-        self.y = coor[1]
-        self.speedX = 300
-        self.speedY = 300
-        self.radius = radius
-        self.color = (200, 150, 100)
+def ball_update(ball, time):
+        ball['x'] += int(ball['speedX']*time)
+        ball['y'] += int(ball['speedY']*time)
+        ball['angle'] += ball['angleSpeed']*time
+       # ball['angle'] -= 2*math.pi*int(ball['angle']/2*math.pi)
 
-    def update(self, pressed_keys, time):
-        #if pressed_keys[K_UP]:
-         #   self.rect.move_ip(0, -self.speedY*time)
-        #if pressed_keys[K_DOWN]:
-         #   self.rect.move_ip(0, self.speedY*time)
-        #if pressed_keys[K_LEFT]:
-         #   self.rect.move_ip(-self.speedX*time, 0)
-        #if pressed_keys[K_RIGHT]:
-         #   self.rect.move_ip(self.speedX*time, 0)
+def ball_colide(ball, wall):
+        if ball['x']-ball['rad'] < 0:
+            ball['x'] = ball['rad']
+            ball['speedX'] = -ball['speedX']
+        elif ball['x'] + ball['rad'] > 800:
+            ball['x'] = 800-ball['rad']
+            ball['speedX'] = -ball['speedX']
+        if ball['y'] - ball['rad'] < 0:
+            ball['y'] = ball['rad']
+            ball['speedY'] = -ball['speedY']
+        elif ball['y'] + ball['rad'] > 600:
+            ball['y'] = 600-ball['rad']
+            ball['speedY'] = -ball['speedY']
 
-        self.x += int(self.speedX*time)
-        self.y += int(self.speedY*time)
-
-    def colide(self):
-        if self.x-self.radius < 0:
-            self.x = self.radius
-            self.speedX = -self.speedX
-        elif self.x+self.radius > 800:
-            self.x = 800-self.radius
-            self.speedX = -self.speedX
-        if self.y-self.radius < 0:
-            self.y = self.radius
-            self.speedY = -self.speedY
-        elif self.y+self.radius > 600:
-            self.y = 600-self.radius
-            self.speedY = -self.speedY
-
-        if self.y-self.radius<wall.rect.bottom:
-            if abs(self.speedY-wall.speed)<1e-6:
-                self.speedY = -self.speedY
+        if ball['y'] - ball['rad'] < wall['y']+wall['height']/2:
+            if abs(ball['speedY']-wall['speedY'])<1e-6:
+                ball['speedY'] = -ball['speedY']
             else:
-                self.speedY = -self.speedY+wall.speed/2
-           # print(self.speedY)
-            self.y = int(wall.rect.bottom+self.radius)
+                ball['speedY'] = -ball['speedY'] #+wall['speedY']
+            ball['y'] = int(wall['y']+wall['height']/2 + ball['rad'])
 
-    def draw(self):
-        pg.draw.circle(screen, self.color, (self.x, self.y), self.radius)
+def ball_draw(ball):
+    pg.draw.circle(screen, ball['color'], (ball['x'], ball['y']), ball['rad'])
+    if (ball['angleSpeed'] != 0):
+        start = (ball['x']+math.cos(ball['angle'])*ball['rad'],
+                 ball['y']+math.sin(ball['angle'])*ball['rad'])
+        end = (ball['x']+math.cos(ball['angle']+math.pi)*ball['rad'],
+               ball['y']+math.sin(ball['angle']+math.pi)*ball['rad'])
+        pg.draw.line(screen, (0, 0, 0), start, end)
 
-class Wall(pg.sprite.Sprite):
-    def __init__(self):
-        super(Wall, self).__init__()
-        self.surf = pg.Surface((SIZE[0], 200))
-        self.surf.fill((0, 0, 0))
-        self.rect = self.surf.get_rect()
-        self.speed = 100 
-
-    def update(self, time):
-        self.rect.move_ip(0, self.speed*time)
-
-        if self.rect.top < 0:
-            self.rect.top = 0
-            self.speed = -self.speed
-        elif self.rect.bottom > 400:
-            self.rect.bottom = 400
-            self.speed = -self.speed
+def wall_update(wall, time):
+        wall['y'] += wall['speedY']*time
         
+        if wall['y']-wall['height']/2 < 0:
+            wall['y'] = wall['height']/2
+            wall['speedY'] = -wall['speedY']
+        elif wall['y']+wall['height']/2 > 400:
+            wall['y'] = 400 - wall['height']/2
+            wall['speedY'] = -wall['speedY']
+
+
+def wall_draw(wall):
+    x, y, height, width = wall['x'], wall['y'], wall['height'], wall['width']
+    rect = pg.Rect((x-width/2, y-height/2), (width, height))
+    pg.draw.rect(screen, wall['color'], rect)
 
 pg.init()
 pg.display.set_caption('Animation by Ryazanov Maxim')
 screen = pg.display.set_mode(SIZE)
-ball = Ball((400, 500))
-wall = Wall()
+
+ball = {'x': 500,
+        'y': 500,
+        'rad': 50,
+        'color': (200, 150, 100),
+        'angleSpeed': math.pi,
+        'speedX': 300,
+        'speedY': 300,
+        'angle': 0,
+        }
+
+wall = {'x': SIZE[0]/2,
+        'y': 200,
+        'height': 200,
+        'width': SIZE[0],
+        'color': (100, 0, 0),
+        'speedY': 150,
+        }
+
 running = True
 clock = pg.time.Clock()
-objects = pg.sprite.Group()
-objects.add(wall)
-surf = pg.Surface((100, 100 ))
-pg.draw.circle(surf, (0, 0, 0), (25,25),10)
-balls = pg.sprite.Group(ball)
 while running:
     for event in pg.event.get():
         if event.type == QUIT:
@@ -92,26 +89,17 @@ while running:
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE:
                 running = False
-        if event.type == MOUSEBUTTONDOWN:
-            balls.add(Ball(event.dict['pos']))
 
     secs = clock.tick(TICKS)/1000
-    #if pg.sprite.spritecollideany(ball, pg.sprite.Group(wall)):
-       # ball.speedY = -ball.speedY
         
-    pressed_keys = pg.key.get_pressed()
-    for i in balls:
-        i.update(pressed_keys, time=secs)
-    wall.update(time=secs)
-    for i in balls:
-        i.colide()
+    ball_update(ball, secs)
+    wall_update(wall, secs)
+
+    ball_colide(ball, wall)
 
     screen.fill(BACKGROUND)
-    for obj in objects:
-        screen.blit(obj.surf, obj.rect)
-    screen.blit(surf, (100, 600))
-    for i in balls:
-        i.draw()
+    wall_draw(wall)
+    ball_draw(ball)
     pg.display.update()
 
 pg.quit()
